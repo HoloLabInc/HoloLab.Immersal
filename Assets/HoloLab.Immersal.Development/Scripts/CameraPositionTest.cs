2c048c30052fb8ddf4a583df309f03a453a21d4a
Furuta Yusuke <furuta.yusuke.012@gmail.com>
2021-04-30 20:08:01 +0900

    Add development files

---
+++ Assets/HoloLab.Immersal.Development/Scripts/CameraPositionTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HoloLab.Immersal;
+using UnityEngine;
+
+public class CameraPositionTest : MonoBehaviour
+{
+    [SerializeField]
+    private ImmersalLocalization immersalLocalization = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        immersalLocalization.OnLocalized += ImmersalLocalization_OnLocalized;
+    }
+
+    private void ImmersalLocalization_OnLocalized(ImmersalLocalization.LocalizeInfo info)
+    {
+        transform.position = info.CameraPose.position;
+        transform.rotation = info.CameraPose.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}

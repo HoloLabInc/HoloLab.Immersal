74b086e73a2b88b9dac9ff392a6364b8ed51ac1f
Furuta Yusuke <furuta.yusuke.012@gmail.com>
2021-06-04 16:18:32 +0900

    Refactor coordinate transformation

---
+++ Assets/HoloLab.Immersal/Scripts/CoordinateUtility.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace HoloLab.Immersal
+{
+    public static class CoordinateUtility
+    {
+        public static Pose Inverse(this Pose pose)
+        {
+            var m = Matrix4x4.TRS(pose.position, pose.rotation, Vector3.one);
+            var inverse = m.inverse;
+
+            var rotation = inverse.ExtractRotation();
+            var position = inverse.ExtractPosition();
+            return new Pose(position, rotation);
+        }
+
+        public static Vector3 ExtractPosition(this Matrix4x4 matrix)
+        {
+            return matrix.GetColumn(3);
+        }
+
+        public static Quaternion ExtractRotation(this Matrix4x4 matrix)
+        {
+            return Quaternion.LookRotation(matrix.GetColumn(2), matrix.GetColumn(1));
+        }
+    }
+}
